f1f61314181d3b641958187c931e9ccfdb9bbab1
Hung <>
2024-11-29 14:13:26 +0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to QuangHung's Java Repository");
+        Welcome();
+    }
+
+    public static void Welcome() {
+        System.out.println("\nEnter your name: ");
+        String s = sc.next();
+        PrintMessage(s);
+    }
+
+    public static void PrintMessage(String name) {
+        System.out.println("Hello " + name);
+        System.out.println("Welcome to my Repository");
     }
 }
 
